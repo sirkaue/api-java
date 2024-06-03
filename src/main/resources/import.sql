773ca6a5e8035d0350e1dda6afb54b0b5a38e58e
sirkaue <kauekdslopes@gmail.com>
2024-06-03 20:13:58 -0300

    seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Kauê', '12345678900', 3000.00, TIMESTAMP WITH TIME ZONE '2024-06-20T20:00:00.00000Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('João', '12345678900', 2430.00, TIMESTAMP WITH TIME ZONE '2023-06-20T20:00:00.00000Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Julia', '12345978900', 2330.00, TIMESTAMP WITH TIME ZONE '2022-06-20T20:00:00.00000Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Julio', '12345328900', 30000.00, TIMESTAMP WITH TIME ZONE '2021-06-20T20:00:00.00000Z', 6);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Renato', '12345878900', 3500.00, TIMESTAMP WITH TIME ZONE '2020-06-20T20:00:00.00000Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Kleber', '12340078900', 5000.00, TIMESTAMP WITH TIME ZONE '2019-06-20T20:00:00.00000Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Larissa', '12364678900', 2000.00, TIMESTAMP WITH TIME ZONE '2018-06-20T20:00:00.00000Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Monteiro', '12115678900', 7000.00, TIMESTAMP WITH TIME ZONE '2017-06-20T20:00:00.00000Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Valverde', '12235678900', 5500.00, TIMESTAMP WITH TIME ZONE '2016-06-20T20:00:00.00000Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Júnior', '12361678900', 3700.00, TIMESTAMP WITH TIME ZONE '2015-06-20T20:00:00.00000Z', 2);
