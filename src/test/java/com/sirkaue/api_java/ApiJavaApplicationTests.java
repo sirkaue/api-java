af18a160db2e6cacdfd4c3d0f3d4bf2442319f18
sirkaue <kauekdslopes@gmail.com>
2024-06-03 17:26:49 -0300

    first commit

---
+++ src/test/java/com/sirkaue/api_java/ApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirkaue.api_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
