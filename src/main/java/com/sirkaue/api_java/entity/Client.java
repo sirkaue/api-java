1388de220b999154ce615ed59a6316947548eb3a
sirkaue <kauekdslopes@gmail.com>
2024-06-03 20:15:45 -0300

    mapping column birthDate

--- src/main/java/com/sirkaue/api_java/entity/Client.java
+++ src/main/java/com/sirkaue/api_java/entity/Client.java
@@ -16,17 +16,20 @@ public class Client implements Serializable {
     private String name;
     private String cpf;
     private Double income;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant birthDate;
     private Integer children;
 
     public Client() {
     }
 
-    public Client(Long id, String name, String cpf, Double income, Integer children) {
+    public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
         this.id = id;
         this.name = name;
         this.cpf = cpf;
         this.income = income;
+        this.birthDate = birthDate;
         this.children = children;
     }
 
