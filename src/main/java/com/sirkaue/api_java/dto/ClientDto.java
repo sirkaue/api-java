780a64df3954c1858a6a3fffadbde5aa87c00d38
sirkaue <kauekdslopes@gmail.com>
2024-06-03 20:14:21 -0300

    DTO

---
+++ src/main/java/com/sirkaue/api_java/dto/ClientDto.java
@@ -0,0 +1,77 @@
+package com.sirkaue.api_java.dto;
+
+import com.sirkaue.api_java.entity.Client;
+import jakarta.persistence.Column;
+
+import java.time.Instant;
+
+public class ClientDto {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private Instant birthDate;
+    private Integer children;
+
+    public ClientDto() {
+    }
+
+    public ClientDto(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDto(Client entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.cpf = entity.getCpf();
+        this.income = entity.getIncome();
+        this.birthDate = entity.getBirthDate();
+        this.children = entity.getChildren();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public Instant getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+}
